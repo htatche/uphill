472e37202a741ec65b43bf55bd04ad15116f38e1
Herve Tatche <herve.tatche@gmail.com>
2026-03-05 09:54:46 +0100

    Add Vite alias config for @/ imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-export default {
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: 5173
     }
-}
+});
